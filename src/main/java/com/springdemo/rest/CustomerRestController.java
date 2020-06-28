d88dad936beb482432163138c68cbc1dcf552333
Mateonidas <teus.polec@gmail.com>
2020-06-28 18:00:41 +0200

    Added get Customers mapping.

--- src/main/java/com/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/springdemo/rest/CustomerRestController.java
@@ -3,10 +3,7 @@ package com.springdemo.rest;
 import com.springdemo.entity.Customer;
 import com.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +27,16 @@ public class CustomerRestController {
         if (customer == null) {
             throw new CustomerNotFoundException("Customer id not found - " + customerId);
         }
+
+        return customer;
+    }
+
+    @RequestMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer){
+
+        //hibernate gives a new index if the index of the object being sent is empty or equal to 0
+        customer.setId(0);
+        customerService.saveCustomer(customer);
 
         return customer;
     }
